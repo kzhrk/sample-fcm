dc0a9fa38b160347578fb229dee95104e6c6d5c9
kzhrk <info@kzhrk.com>
2022-08-02 17:05:26 +0900

    cors test

--- static/firebase-messaging-sw.js
+++ static/firebase-messaging-sw.js
@@ -8,23 +8,29 @@ self.addEventListener('push', function (event) {
   );
 });
 
-self.addEventListener('notificationclick', function(event) {
-    let url = 'https://example.com/';
-    event.notification.close(); // Android needs explicit close.
-    event.waitUntil(
-        clients.matchAll({type: 'window'}).then( windowClients => {
-            // Check if there is already a window/tab open with the target URL
-            for (var i = 0; i < windowClients.length; i++) {
-                var client = windowClients[i];
-                // If so, just focus it.
-                if (client.url === url && 'focus' in client) {
-                    return client.focus();
-                }
-            }
-            // If not, then open the target URL in a new window/tab.
-            if (clients.openWindow) {
-                return clients.openWindow(url);
-            }
-        })
-    );
+self.addEventListener('notificationclick', function (event) {
+  const url = 'https://example.com/';
+  event.notification.close(); // Android needs explicit close.
+  event.waitUntil(
+    clients.matchAll({ type: 'window' }).then(windowClients => {
+      // Check if there is already a window/tab open with the target URL
+      for (let i = 0; i < windowClients.length; i++) {
+        const client = windowClients[i];
+        // If so, just focus it.
+        console.log(client.url)
+        if (/kakari-pharmacy\.medpeer\.jp/.test(client.url) && 'focus' in client) {
+          client.focus();
+          client.postMessage({
+            type: 'PLAY_AUDIO',
+            audioType: 'new-message'
+          })
+          return
+        }
+      }
+      // If not, then open the target URL in a new window/tab.
+      if (clients.openWindow) {
+        return clients.openWindow(url);
+      }
+    })
+  );
 });
